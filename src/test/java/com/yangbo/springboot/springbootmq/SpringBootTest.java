60133706365b271b8f900a4d3c280949807d025e
yangbo <yangbogod@gmail.com>
2018-04-21 13:36:34 +0800

    带有附件的邮件发送

--- src/test/java/com/yangbo/springboot/springbootmq/SpringBootTest.java
+++ src/test/java/com/yangbo/springboot/springbootmq/SpringBootTest.java
@@ -3,9 +3,14 @@ package com.yangbo.springboot.springbootmq;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 /**
  * yangbo
@@ -18,15 +23,28 @@ public class SpringBootTest {
     @Autowired
     private JavaMailSender javaMailSender;
     @Test
-    public void sendmail(){
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom("1370151513@qq.com");
-        simpleMailMessage.setTo("2252625006@qq.com");
-        simpleMailMessage.setSubject("测试油价");
-        simpleMailMessage.setText("lallalal");
-        javaMailSender.send(simpleMailMessage);
+    public void sendmail() throws MessagingException {
+        /**
+         * 简单发送
+         */
+        //SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        //simpleMailMessage.setFrom("1370151513@qq.com");
+        //simpleMailMessage.setTo("2252625006@qq.com");
+        //simpleMailMessage.setSubject("测试油价");
+        //simpleMailMessage.setText("lallalal");
+        //javaMailSender.send(simpleMailMessage);
+        /*
+         * 带附件的邮件发送
+         */
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 
-
-
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,true);
+        mimeMessageHelper.setFrom("1370151513@qq.com");
+        mimeMessageHelper.setTo("2252625006@qq.com");
+        mimeMessageHelper.setSubject("邮件，带有附件");
+        mimeMessageHelper.setText("正文，请查收");
+        FileSystemResource fileSystemResource = new FileSystemResource(new File("/Users/yangbo/Desktop/WX20180421-130339@2x.png"));
+        mimeMessageHelper.addAttachment("附件",fileSystemResource);
+        javaMailSender.send(mimeMessage);
     }
 }
