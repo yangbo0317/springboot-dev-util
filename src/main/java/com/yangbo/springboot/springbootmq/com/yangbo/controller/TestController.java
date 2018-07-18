1f3bdfa089f309153282989b047198d37dcc5866
yangbo <yangbogod@gmail.com>
2018-07-18 10:10:02 +0800

    springboot整合mybatis

--- src/main/java/com/yangbo/springboot/springbootmq/com/yangbo/controller/TestController.java
+++ src/main/java/com/yangbo/springboot/springbootmq/com/yangbo/controller/TestController.java
@@ -1,13 +1,23 @@
 package com.yangbo.springboot.springbootmq.com.yangbo.controller;
 
+import com.yangbo.springboot.springbootmq.com.yangbo.mapper.UserMapper;
 import com.yangbo.springboot.springbootmq.com.yangbo.rabbitmq.provider.QueueEnum;
 import com.yangbo.springboot.springbootmq.com.yangbo.rabbitmq.provider.RabbitMqSender;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * yangbo
@@ -18,6 +28,9 @@ import javax.annotation.Resource;
 public class TestController {
     @Resource
     private RabbitMqSender rabbitMqSender;
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private UserMapper userMapper;
     @RequestMapping(value = "hello")
     public String testHello(@RequestBody User user){
         try {
@@ -29,4 +42,17 @@ public class TestController {
         System.out.println();
         return "hello";
     }
+
+    @RequestMapping(value="adduser")
+    public String addUser(@RequestBody User user){
+        userMapper.insert(user.getName(),user.getGender());
+        return "";
+    }
+    @RequestMapping(value="getUser")
+    public String getUser(@RequestParam(value="name") String name){
+        List<User> list =userMapper.findByName(name);
+        System.out.println(list.size());
+        return "";
+    }
+
 }
--- src/main/java/com/yangbo/springboot/springbootmq/com/yangbo/controller/User.java
+++ src/main/java/com/yangbo/springboot/springbootmq/com/yangbo/controller/User.java
@@ -1,26 +1,21 @@
 package com.yangbo.springboot.springbootmq.com.yangbo.controller;
 
+import lombok.Data;
+
+import java.util.Date;
+
 /**
  * yangbo
  * 2018-07-13
  * springboot-dev-util
  **/
+@Data
 public class User {
+    private Long id;
     private String name;
-    private Student student;
-    public String getName() {
-        return name;
-    }
+    private Integer gender;
+    private String adsress;
+    private Date createTime;
+    private Date updateTime;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Student getStudent() {
-        return student;
-    }
-
-    public void setStudent(Student student) {
-        this.student = student;
-    }
 }
