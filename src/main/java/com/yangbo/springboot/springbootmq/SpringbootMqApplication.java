af96a446dc2c53c584b90876b57ded975634fc5f
yangbo <yangbogod@gmail.com>
2018-07-18 16:26:24 +0800

    springboot整合swagger初版

--- src/main/java/com/yangbo/springboot/springbootmq/SpringbootMqApplication.java
+++ src/main/java/com/yangbo/springboot/springbootmq/SpringbootMqApplication.java
@@ -2,11 +2,39 @@ package com.yangbo.springboot.springbootmq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringbootMqApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMqApplication.class, args);
+	}
+	@Bean
+	public Docket createRestApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.yangbo.springboot.springbootmq.com.yangbo.controller"))
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("yb-cloud接口文档")
+				.description("接口文档")
+				.termsOfServiceUrl("https://github.com/yangbo0317")
+				.contact("YangBo")
+				.version("1.0")
+				.build();
 	}
 }
--- src/main/java/com/yangbo/springboot/springbootmq/com/yangbo/controller/UserController.java
+++ src/main/java/com/yangbo/springboot/springbootmq/com/yangbo/controller/UserController.java
@@ -2,13 +2,14 @@ package com.yangbo.springboot.springbootmq.com.yangbo.controller;
 
 import com.yangbo.springboot.springbootmq.com.yangbo.mapper.UserMapper;
 import com.yangbo.springboot.springbootmq.com.yangbo.utils.JackJsonUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +20,7 @@ import java.util.List;
  **/
 @RestController
 @RequestMapping("/user")
+@Api("UserController相关api")
 public class UserController {
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
@@ -27,13 +29,18 @@ public class UserController {
     private JackJsonUtil jackJsonUtil;
 
     private static Logger LOGGER = LoggerFactory.getLogger(UserController.class);
-
-    @RequestMapping(value="addUser")
+    @ApiOperation("用户注册")
+    @RequestMapping(value="addUser",method=RequestMethod.POST)
     public String addUser(@RequestBody User user){
         userMapper.insert(user.getName(),user.getGender());
         return "";
     }
-    @RequestMapping(value="getUser")
+    @ApiOperation("获取用户信息")
+    @RequestMapping(value="getUser",method= RequestMethod.GET)
+    @ApiResponses({
+            @ApiResponse(code=400,message="请求参数没填好"),
+            @ApiResponse(code=404,message="请求路径没有或页面跳转路径不对")
+    })
     public String getUser(@RequestParam(value="name") String name){
         LOGGER.info("name="+name);
         List<User> list =userMapper.findByName(name);
